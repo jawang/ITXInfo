2e6c49b7473610193c510649d3bc061763112999
jawang <thejasonwang@outlook.com>
2013-07-18 11:34:40 -0700

    overwrite popup implemented

--- itx-info.py
+++ itx-info.py
@@ -28,7 +28,7 @@ class Application(tk.Frame):
         self.updateButton = tk.Button(command=self.update,text='Update')
         self.updateButton.grid(row=2,column=0,columnspan=3)
 
-    def update(self):
+    def update(self,event=None):
         # Check if necessary folder exists
         if not os.path.isdir('Keep Out'):
             os.mkdir('Keep Out')
@@ -38,7 +38,7 @@ class Application(tk.Frame):
         # Check if Excel file exists
         if not os.path.isfile('Master.xlsx'):
             workbook = xl.Workbook()
-            worksheet = workbook.create_sheet()
+            worksheet = workbook.get_active_sheet()
             worksheet.title = 'ITX'
             headers = ['ITX','A-Version','A-Hardware','B-Version','B-Hardware']
             for i in range(5):
@@ -50,63 +50,107 @@ class Application(tk.Frame):
         worksheet = workbook.get_sheet_by_name(name = 'ITX')
 
         itx = str(self.inputs[0].get())
+        if itx == '':
+            os.chdir('..')
+            return
         if string.lower(itx)[-1] == 'a':
-            print 'asdf'
-            i = 1
-            exists = False
-            while worksheet.cell(row=i,column=0).value != None:
-                if str(worksheet.cell(row=i,column=0).value) == itx[:-1]:
-                    worksheet.cell(row=i,column=1).value = \
-                                str(self.inputs[1].get())
-                    worksheet.cell(row=i,column=2).value = \
-                                str(self.inputs[2].get())
-                    exists = True
-                    break
-                i += 1
-
-            if not exists:
-                worksheet.cell(row=i,column=0).value = \
-                                str(self.inputs[0].get())[:-1]
-                worksheet.cell(row=i,column=1).value = \
-                                str(self.inputs[1].get())
-                worksheet.cell(row=i,column=2).value = \
-                                str(self.inputs[2].get())
-
-            workbook.save(filename='Master.xlsx')
+            self.writeline(1,2,workbook,itx)
                 
         elif string.lower(itx)[-1] == 'b':
-            i = 1
-            exists = False
-            while worksheet.cell(row=i,column=0).value != None:
-                if str(worksheet.cell(row=i,column=0).value) == itx[:-1]:
-                    worksheet.cell(row=i,column=3).value = \
-                                str(self.inputs[1].get())
-                    worksheet.cell(row=i,column=4).value = \
-                                str(self.inputs[2].get())
-                    exists = True
-                    break
-                i += 1
-
-            if not exists:
-                worksheet.cell(row=i,column=0).value = \
-                                str(self.inputs[0].get())[:-1]
-                worksheet.cell(row=i,column=3).value = \
-                                str(self.inputs[1].get())
-                worksheet.cell(row=i,column=4).value = \
-                                str(self.inputs[2].get())
-        
-            workbook.save(filename='Master.xlsx')
+            self.writeline(3,4,workbook,itx)
 
         else:
             print 'Invalid input: ITX'
             
         os.chdir('..')
 
+    # Handle main or backup cases
+    def writeline(self,a,b,workbook,itx):
+        worksheet = workbook.get_sheet_by_name(name = 'ITX')
+        i = 1
+        exists = False
+        while worksheet.cell(row=i,column=0).value != None:
+
+            # Checks if ITX entry exists
+            if str(worksheet.cell(row=i,column=0).value) == itx[:-1]:
+
+                # Opens popup to confirm overwrite
+                if worksheet.cell(row=i,column=a).value != None or \
+                   worksheet.cell(row=i,column=b).value != None:
+
+                    popupstring = 'Existing entry:\nITX:'+\
+                            string.upper(itx)+' V: '+\
+                            str(worksheet.cell(row=i,column=a).value)+' H: '+\
+                            str(worksheet.cell(row=i,column=b).value)+'.'+\
+                            '\n\nOverwrite?\n'
+                    inputDialog = Popup(root,popupstring)
+                    root.wait_window(inputDialog.top)
+
+                try:
+                    if overwrite:
+                        worksheet.cell(row=i,column=a).value = \
+                                str(self.inputs[1].get())
+                        worksheet.cell(row=i,column=b).value = \
+                                str(self.inputs[2].get())
+                        popupstring = 'Successfully entered:\n\nITX:'+\
+                            string.upper(itx)+' V: '+\
+                            str(worksheet.cell(row=i,column=a).value)+' H: '+\
+                            str(worksheet.cell(row=i,column=b).value)+'.\n'
+                        successDialog = Success(root,popupstring)
+                        root.wait_window(successDialog.top)
+                except Exception:
+                    ''
+                exists = True
+                break
+            i += 1
+
+        if not exists:
+            worksheet.cell(row=i,column=0).value = \
+                            str(self.inputs[0].get())[:-1]
+            worksheet.cell(row=i,column=a).value = \
+                            str(self.inputs[1].get())
+            worksheet.cell(row=i,column=b).value = \
+                            str(self.inputs[2].get())
+            popupstring = 'Successfully entered:\n\nITX:'+\
+                            string.upper(itx)+' V: '+\
+                            str(worksheet.cell(row=i,column=a).value)+' H: '+\
+                            str(worksheet.cell(row=i,column=b).value)+'.\n'
+            successDialog = Success(root,popupstring)
+            root.wait_window(successDialog.top)
+        workbook.save(filename='Master.xlsx')
+
+
+        
 class Popup:
-    def __init__(self,parent):
-        ''
-        
-        
+    def __init__(self, parent, popupstring):
+        top = self.top = tk.Toplevel(parent)
+        self.myLabel = tk.Label(top, text=popupstring)
+        self.myLabel.grid(column=0,row=0,columnspan=2)
+
+        self.yesButton = tk.Button(top, text='Yes',
+                                   command=lambda : self.send(True))
+        self.yesButton.grid(row=1,column=0,sticky=tk.E)
+        self.noButton = tk.Button(top, text='No',
+                                  command=lambda : self.send(False))
+        self.noButton.grid(row=1,column=1,sticky=tk.W)
+
+    def send(self,doit):
+        global overwrite
+        overwrite = doit
+        self.top.destroy()
+
+class Success:
+    def __init__(self, parent, popupstring):
+        top = self.top = tk.Toplevel(parent)
+        self.myLabel = tk.Label(top, text=popupstring)
+        self.myLabel.grid(column=0,row=0)
+
+        self.closeButton = tk.Button(top, text='Close',
+                                   command=self.send)
+        self.closeButton.grid(row=1,column=0)
+
+    def send(self):
+        self.top.destroy()
     
 # MAIN PROCESS
 root = tk.Tk()
@@ -114,7 +158,7 @@ root = tk.Tk()
 #root.resizable(0,0)
 #root.minsize(width=550,height=450)
 app = Application()
-#root.bind("<Configure>",app.resize)
+#root.bind("<Return>",app.update)
 app.master.title('ITX Info')
 
 app.mainloop()
