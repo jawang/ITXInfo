e18bd9b1490cf07cf14e3f2829c82d50d96a16aa
jawang <thejasonwang@outlook.com>
2013-07-19 17:15:55 -0700

    Revert "Revert "Revert "new inputs"""
    This reverts commit 710a38676c6ce3698e768b469156ac4695700ec6.

--- itx-info.py
+++ itx-info.py
@@ -9,6 +9,7 @@ class Application(tk.Frame):
     def __init__(self,master=None):
 
         tk.Frame.__init__(self,master)
+
         self.grid()
         self.createWidgets()
 
@@ -82,24 +83,21 @@ class Application(tk.Frame):
         
         
         # Inputs #################################
-        self.inputText = ['#','A/B', 'Version', 'Hardware']
-        self.inputs = [tk.StringVar() for i in range(4)]
-        self.inputs[0] = tk.IntVar()
-        self.inputLab = [tk.Label(self,text=self.inputText[i],width=10)
-                         for i in range(4)]
-        #self.inputBox = [tk.Entry(self,textvariable=self.inputs[i])
-        #                 for i in range(3)]
-        self.itxID = tk.Entry(self,textvariable=self.inputs[0],width=10)
+        self.inputText = ['ITX ID', 'Version', 'Hardware']
+        self.inputs = [tk.StringVar() for i in range(3)]
+        self.inputLab = [tk.Label(self,text=self.inputText[i])
+                         for i in range(3)]
+        self.inputBox = [tk.Entry(self,textvariable=self.inputs[i])
+                         for i in range(3)]
 
-        for i in range(4):
+        for i in range(3):
             self.inputLab[i].grid(row=1,column=i)
-            #self.inputBox[i].grid(row=2,column=i)
-        self.itxID.grid(row=2,column=0)
+            self.inputBox[i].grid(row=2,column=i)
 
         # NEW INPUTS #############################
         self.AorBstr = tk.StringVar()
-        self.AorB = tk.OptionMenu(self,self.inputs[1],'A','B')
-        self.AorB.grid(row=2,column=1)
+        self.AorB = tk.OptionMenu(self,self.AorBstr,'A','B')
+        self.AorB.grid(row=4,column=0)
         
         self.versionstr = tk.StringVar()
         workbook = xl.load_workbook('config.xlsx')
@@ -109,9 +107,9 @@ class Application(tk.Frame):
         while worksheet.cell(row=i,column=0).value != None:
             versionopts.append(worksheet.cell(row=i,column=0).value)
             i += 1
-        self.version = tk.OptionMenu(self,self.inputs[2],
+        self.version = tk.OptionMenu(self,self.versionstr,
                                      *(tuple(versionopts)))
-        self.version.grid(row=2,column=2)
+        self.version.grid(row=4,column=1)
         
         self.hardwarestr = tk.StringVar()
         i = 1
@@ -119,13 +117,13 @@ class Application(tk.Frame):
         while worksheet.cell(row=i,column=1).value != None:
             hardwareopts.append(worksheet.cell(row=i,column=1).value)
             i += 1
-        self.hardware = tk.OptionMenu(self,self.inputs[3],
+        self.hardware = tk.OptionMenu(self,self.hardwarestr,
                                       *(tuple(hardwareopts)))
-        self.hardware.grid(row=2,column=3)
+        self.hardware.grid(row=4,column=2)
         
         # Update button ##########################
         self.updateButton = tk.Button(command=self.update,text='Update')
-        self.updateButton.grid(row=3,column=0,columnspan=3,pady=10)
+        self.updateButton.grid(row=3,column=0,columnspan=3)
 
     def update(self,event=None):
         # Check if necessary folder exists
@@ -148,82 +146,68 @@ class Application(tk.Frame):
         workbook = xl.load_workbook(filename = 'Master.xlsx')
         worksheet = workbook.get_sheet_by_name(name = 'ITX')
 
-        try:
-            itx = self.inputs[0].get()
-        except Exception:
-            os.chdir('..')
-            errorDialog = Dialog(root,'Error: Please enter a valid #')
-            root.wait_window(errorDialog.top)
-            return
-        AorB = self.inputs[1].get()
-        #print AorB
-        #print itx
-        '''
+        itx = str(self.inputs[0].get())
         if itx == '':
             os.chdir('..')
-            return'''
-        if string.lower(AorB) == 'a':
+            return
+        if string.lower(itx)[-1] == 'a':
             self.writeline(1,2,workbook,itx)                
-        elif string.lower(AorB) == 'b':
+        elif string.lower(itx)[-1] == 'b':
             self.writeline(3,4,workbook,itx)
+        else:
+            print 'Invalid input: ITX'
             
         os.chdir('..')
 
     # Handle main or backup cases
     def writeline(self,a,b,workbook,itx):
-        AorB = self.inputs[1].get()
         worksheet = workbook.get_sheet_by_name(name = 'ITX')
         i = 1
         exists = False
         while worksheet.cell(row=i,column=0).value != None:
 
             # Checks if ITX entry exists
-            if int(worksheet.cell(row=i,column=0).value) == itx:
+            if str(worksheet.cell(row=i,column=0).value) == itx[:-1]:
 
                 # Opens popup to confirm overwrite
                 if worksheet.cell(row=i,column=a).value != None or \
                    worksheet.cell(row=i,column=b).value != None:
 
                     popupstring = 'Existing entry:\nITX:'+\
-                            str(itx)+string.upper(AorB)+' V: '+\
+                            string.upper(itx)+' V: '+\
                             str(worksheet.cell(row=i,column=a).value)+' H: '+\
                             str(worksheet.cell(row=i,column=b).value)+'.'+\
                             '\n\nOverwrite?\n'
                     inputDialog = Popup(root,popupstring)
                     root.wait_window(inputDialog.top)
-                else:
-                    global overwrite
-                    overwrite = True
 
-                #try:
-                if overwrite:
-                    if str(self.inputs[2].get()) != '':
+                try:
+                    if overwrite:
                         worksheet.cell(row=i,column=a).value = \
-                            str(self.inputs[2].get())
-                    if str(self.inputs[3].get()) != '':
+                                str(self.inputs[1].get())
                         worksheet.cell(row=i,column=b).value = \
-                            str(self.inputs[3].get())
-                    popupstring = 'Successfully entered:\n\nITX:'+\
-                        str(itx)+string.upper(AorB)+' V: '+\
-                        str(worksheet.cell(row=i,column=a).value)+' H: '+\
-                        str(worksheet.cell(row=i,column=b).value)+'.\n'
-                    successDialog = Dialog(root,popupstring)
-                    root.wait_window(successDialog.top)
-                #except Exception:
-                    #''
+                                str(self.inputs[2].get())
+                        popupstring = 'Successfully entered:\n\nITX:'+\
+                            string.upper(itx)+' V: '+\
+                            str(worksheet.cell(row=i,column=a).value)+' H: '+\
+                            str(worksheet.cell(row=i,column=b).value)+'.\n'
+                        successDialog = Dialog(root,popupstring)
+                        root.wait_window(successDialog.top)
+                except Exception:
+                    ''
                 exists = True
                 break
             i += 1
 
         if not exists:
             worksheet.cell(row=i,column=0).value = \
-                            str(self.inputs[0].get())
+                            str(self.inputs[0].get())[:-1]
             worksheet.cell(row=i,column=a).value = \
-                            str(self.inputs[2].get())
+                            str(self.inputs[1].get())
             worksheet.cell(row=i,column=b).value = \
-                            str(self.inputs[3].get())
+                            str(self.inputs[2].get())
             popupstring = 'Successfully entered:\n\nITX:'+\
-                            str(itx)+string.upper(AorB)+' V: '+\
+                            string.upper(itx)+' V: '+\
                             str(worksheet.cell(row=i,column=a).value)+' H: '+\
                             str(worksheet.cell(row=i,column=b).value)+'.\n'
             successDialog = Dialog(root,popupstring)
@@ -251,7 +235,6 @@ class Popup:
         global overwrite
         overwrite = doit
         self.top.destroy()
-        #print overwrite
 
 class Dialog:
     def __init__(self, parent, popupstring):
